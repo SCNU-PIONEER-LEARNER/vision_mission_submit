9861b9569d84e46e53e5b208aa97308d26f18f38
18926294706 <1046439512@qq.com>
2025-09-26 18:33:36 +0800

    Update Robot.cpp

--- Robot.cpp
+++ Robot.cpp
@@ -5,9 +5,15 @@ int n;
 Robot::Robot(const char type[],int life,int attack,double hit_rate):type(type[n]),life(life),hit_rate(hit_rate){}
 Building::Building(const char building_type[],int building_life):building_type(building_type[n]),building_life(building_life){}
 
-bool Survive(){
+bool Building::Survive(){
     return true;
 }
-bool Invincible=Survive();
-bool Protected=Survive();
+bool Robot::Survive(){
+    return true;
+}
+bool Invincible=true;
+bool Protected=true;
 bool Health_=true;
+
+void Robot::Hit(Building building_target){};
+void Robot::Hit(Robot robot_target){};
